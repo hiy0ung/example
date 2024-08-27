fc7039c52e3ca9755ec5eaef9e103daed26662be
hay0ung <hyejee98@gmail.com>
2024-08-27 15:47:14 +0900

    20240827

--- e_react/src/pages/a_basic/index.tsx
+++ e_react/src/pages/a_basic/index.tsx
@@ -3,6 +3,9 @@ import Props01 from "./d_Props/Props01";
 import Props02 from "./d_Props/Props02";
 import Rendering01 from "./f_Rendering/Rendering01";
 import Rendering02 from "./f_Rendering/Rendering02";
+import Handler01 from "./g_Handler/Handler01";
+import Handler02, { ChildComponent } from "./g_Handler/Handler02";
+import Handler03 from "./g_Handler/Handler03";
 
 // import ReactExample from './pages/a_basic/a_React/ReactExample';
 
@@ -34,6 +37,12 @@ export default function index() {
       <h2>f_Rendering 리액트의 조건부 랜더링</h2>
       <Rendering01 />
       <Rendering02 />
+
+      <h2>g_Handler 리액트의 이벤트 핸들러</h2>
+      <Handler01 />
+      <Handler02 />
+
+      <Handler03 />
     </div>
   );
 }
